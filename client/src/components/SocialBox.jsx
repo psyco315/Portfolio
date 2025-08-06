57e6119c709e0cf91e2f91047f91bcb5e1c4b1f8
psyco315 <parth2004batman@gmail.com>
2025-08-06 16:33:13 +0530

    Landing Page retouch

--- client/src/components/SocialBox.jsx
+++ client/src/components/SocialBox.jsx
@@ -7,21 +7,43 @@ import linkP from '../assets/Linkedin.png'
 const SocialBox = () => {
     return (
         <div className="grid grid-cols-2 gap-4">
-            <a href="https://www.linkedin.com/in/parth-sarathi-yadav-738b87261/" target="_blank" rel="noopener noreferrer">
+            <a
+                href="https://www.linkedin.com/in/parth-sarathi-yadav-738b87261/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="transition-transform duration-100 hover:scale-105"
+            >
                 <img src={linkP} alt="LinkedIn" className="w-[150px] h-[150px]" />
             </a>
 
-            <a href="https://github.com/psyco315" target="_blank" rel="noopener noreferrer">
+            <a
+                href="https://github.com/psyco315"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="transition-transform duration-100 hover:scale-105"
+            >
                 <img src={gitP} alt="GitHub" className="w-[150px] h-[150px]" />
             </a>
 
-            <img src={resumeP} alt="Resume" className="w-[150px] h-[150px]" />
-
-            <a href="mailto:parth2004batman@gmail.com">
-                <img src={emailP} alt="Email" className="w-[150px] h-[150px]" />
+            <a
+                href="https://drive.google.com/file/d/1EPKTVHkSwz2_DR2MYjoZXSIkpY-zQsfC/view?usp=sharing"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="transition-transform duration-100 hover:scale-105"
+            >
+                <img src={resumeP} alt="Resume" className="w-[150px] h-[150px]" />
             </a>
 
+            <a
+                href="https://mail.google.com/mail/?view=cm&fs=1&to=parth315sy@gmail.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="transition-transform duration-100 hover:scale-105"
+            >
+                <img src={emailP} alt="Email" className="w-[150px] h-[150px]" />
+            </a>
         </div>
+
 
     )
 }
